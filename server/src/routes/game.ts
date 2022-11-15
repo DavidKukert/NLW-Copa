c02da8735be8fb606b0db0faf45396577def3ac6
DavidKukert <dkukert@kukert.com>
2022-11-14 21:30:42 -0300

    App construido até os passos da ultima aula

---
+++ server/src/routes/game.ts
@@ -0,0 +1,69 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
+
+export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    "/pools/:id/games",
+    { onRequest: [authenticate] },
+    async (request) => {
+      const getPoolsParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPoolsParams.parse(request.params);
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: request.user.sub,
+                poolId: id,
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
+
+  fastify.post(
+    "/games",
+    { onRequest: [authenticate] },
+    async (request, reply) => {
+      const createGamesBody = z.object({
+        date: z.string(),
+        firstTeamCountryCode: z.string().max(2).min(2),
+        secondTeamCountryCode: z.string().max(2).min(2),
+      });
+
+      const { date, firstTeamCountryCode, secondTeamCountryCode } =
+        createGamesBody.parse(request.body);
+
+      await prisma.game.create({
+        data: {
+          date,
+          firstTeamCountryCode,
+          secondTeamCountryCode,
+        },
+      });
+
+      return reply.status(201).send();
+    }
+  );
+}
